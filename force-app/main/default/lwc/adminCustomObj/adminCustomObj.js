db79de449913b4635ec945883c2c3ebb9310f07c
Nam Phan <namphan56@gmail.com>
2022-12-08 16:36:37 -0500

    added adminCustomObj button

---
+++ force-app/main/default/lwc/adminCustomObj/adminCustomObj.js
@@ -0,0 +1,26 @@
+import { LightningElement,track } from 'lwc';
+import PICTUREZIPLOC from '@salesforce/resourceUrl/AdminCustomObjectPics';
+
+export default class ModalPopupLWC extends LightningElement {
+    //Boolean tracked variable to indicate if modal is open or not default value is false as modal is closed when page is loaded 
+    @track isModalOpen = false;
+
+    buttonName = 'Custom Objects'
+    titleLink = '';
+    pic1 = PICTUREZIPLOC + '/image.png';
+    pic2 = PICTUREZIPLOC + '/image (1).png';
+
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
+    }
+}

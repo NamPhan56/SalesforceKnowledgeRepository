322197b1a694c5edfc181ad3a192074cd1525bd3
Nam Phan <namphan56@gmail.com>
2022-12-08 00:04:21 -0500

    added an admin harness and profiles button

---
+++ force-app/main/default/lwc/adminProfiles/adminProfiles.js
@@ -0,0 +1,24 @@
+import { LightningElement,track } from 'lwc';
+import ADMINPROFILEPIC from '@salesforce/resourceUrl/AdminProfilePic';
+
+export default class ModalPopupLWC extends LightningElement {
+    //Boolean tracked variable to indicate if modal is open or not default value is false as modal is closed when page is loaded 
+    @track isModalOpen = false;
+
+    listPict = ADMINPROFILEPIC + '/List.png';
+    locationPict = ADMINPROFILEPIC+ '/Location.png';
+
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
+    }
+}

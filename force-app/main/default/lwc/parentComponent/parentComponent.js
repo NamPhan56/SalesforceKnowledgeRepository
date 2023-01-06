f74dc8a923932a1fd32b84526a3b70de0d83a0b7
Nam Phan <namphan56@gmail.com>
2023-01-06 07:24:08 -0500

    added parent-to-child and child-to-parent communications

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -2,10 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
 
+    //child to parent
     message;
 
-    parentHandler(event) {
+    childToParentHandler(event) {
         this.message = event.detail;
     }
 
+
+    //parent to child handler
+    parentToChildHandler() {
+        this.template.querySelector('c-child-component').greet('Hey, This Message Is Sent From Parent!');
+    }
 }

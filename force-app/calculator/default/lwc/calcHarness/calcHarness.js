f016822c96ab7c99e7383d06a4ee22ed2c707cf3
Nam Phan <namphan56@gmail.com>
2023-01-09 04:45:14 -0500

    working on validating the formula string for the equals button

--- force-app/calculator/default/lwc/calcHarness/calcHarness.js
+++ force-app/calculator/default/lwc/calcHarness/calcHarness.js
@@ -1,12 +1,33 @@
-import { LightningElement} from 'lwc';
+import { LightningElement, track} from 'lwc';
 
 export default class CalcHarness extends LightningElement {
 
-    input;
+    @track input = "";
+    @track result = "result";
 
-    calculate(i){
-        console.log(i);
+    calculate(e){
+        //this.input = this.template.querySelector('#input1').getAttribute("value");
+        //this.result = "result";
+        let ret = 0;
+        let arraystr;
+        
+        let operations = new Set(["/","*","-","+"]);
+
+        //this.input = this.input.replace(" ", "");
+
+        // for(let i=0; i<this.input.length;i++){
+
+        // }
+        //console.log("input: " + this.input + "\n" + "result: " + this.result);
+
     }
 
+    handleClear(e){
+        this.input = "";
+        this.result = "";
+    }
 
+    handleChange(e){
+        this.input = e.detail.value;
+    }
 }
--- force-app/calculator/default/lwc/inputButtons/inputButtons.js
+++ force-app/calculator/default/lwc/inputButtons/inputButtons.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api} from 'lwc';
 
-export default class InputButtons extends LightningElement {}
+export default class InputButtons extends LightningElement {
+
+    equalHandler(){
+        const evt = new CustomEvent('sendequal', {});
+        this.dispatchEvent(evt);
+    }
+
+    clearHandler(){
+        const evt = new CustomEvent('sendclear', {});
+        this.dispatchEvent(evt);
+    }
+}

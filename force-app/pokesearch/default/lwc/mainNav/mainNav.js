11fd0200773ed56f3329a3f7b6575c965b1fc65a
Nam Phan <namphan56@gmail.com>
2023-01-21 16:46:48 -0500

    creating standard harness, cssLibrary, and main nav. Not finished yet.

---
+++ force-app/pokesearch/default/lwc/mainNav/mainNav.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class MainNav extends LightningElement {}
--- force-app/pokesearch/lwc/pokesearchharness/pokesearchharness.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Pokesearchharness extends LightningElement {}
